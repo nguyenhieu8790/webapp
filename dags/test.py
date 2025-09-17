05b988f806f17f3a284e406a147744465591bb16
nguyenhieu8790 <nguyenhieu8790@gmail.com>
2025-09-17 07:54:52 +0700

    Update test.py

--- dags/test.py
+++ dags/test.py
@@ -1,5 +1,6 @@
 from airflow import DAG
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+
 from datetime import datetime, timedelta
 
 
